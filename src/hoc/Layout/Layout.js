df352655f7638ba6fa85e7d62cbcc4a4c1bb6308
LiliaRibac <ribac.lilia@gmai.com>
2020-04-21 12:43:19 -0700

    add all code for the project in React

---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import Aux from '../ReactAux/ReactAux';
+import classes from './Layout.css';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar'
+import SideDrawer from '../../components/Navigation/SideDrawer/SiteDrawer';
+
+class Layout extends Component {
+  state = {
+    showSideDrawer: true
+  };
+
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+
+  sideDrawerClosedHandler = () => {
+    this.setState(prevState => {
+      return { showSideDrawer: !prevState.showSideDrawer };
+    });
+  };
+  render() {
+    return (
+      <Aux>
+        <Toolbar drawerToggleClicked={this.sideDrawerClosedHandler} />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
+}
+
+export default Layout;
