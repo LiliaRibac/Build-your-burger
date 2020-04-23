a0ebb858fff81d83acba7b8b6feaf547a7fe3279
LiliaRibac <ribac.lilia@gmai.com>
2020-04-23 14:45:08 -0700

    fetching orders(via Redux)

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -3,7 +3,7 @@ import * as actionTypes from '../actions/actionTypes';
 const initialState = {
   orders: [],
   loading: false,
-  purchased :false,
+  purchased: false,
 };
 
 const reducer = (state = initialState, action) => {
@@ -13,10 +13,10 @@ const reducer = (state = initialState, action) => {
   };
   switch (action.type) {
     case actionTypes.PURCHASE_INIT:
-      return{
+      return {
         ...state,
-        purchased:false
-      }
+        purchased: false,
+      };
     case actionTypes.PURCHASE_BURGER_START:
       return {
         ...state,
@@ -27,7 +27,7 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         loading: false,
-        purchased:true,
+        purchased: true,
         orders: state.orders.concat(newOrder),
       };
     case actionTypes.PURCHASE_BURGER_FAIL:
@@ -35,6 +35,24 @@ const reducer = (state = initialState, action) => {
         ...state,
         loading: false,
       };
+
+    case actionTypes.FETCH_ORDERS_START:
+      return {
+        ...state,
+        loading: true,
+      };
+    case actionTypes.FETCH_ORDERS_SUCCESS:
+      return {
+        ...state,
+        orders: action.orders,
+        loading:false
+      };
+      case actionTypes.FETCH_ORDERS_FAIL:
+        return{
+          ...state,
+          loading:false
+        }
+
     default:
       return state;
   }
