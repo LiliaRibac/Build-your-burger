ac6a35c38b7246c9f62a43f4de9ed2c3676ee95f
LiliaRibac <ribac.lilia@gmai.com>
2020-04-25 20:04:12 -0700

    displaying user specific orders

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -16,18 +16,17 @@ export const purchaseBurgerFail = (error) => {
   };
 };
 
-
-export const purchaseBurgerStart =() =>{
+export const purchaseBurgerStart = () => {
   return {
-    type:actionTypes.PURCHASE_BURGER_START
-  }
-}
+    type: actionTypes.PURCHASE_BURGER_START,
+  };
+};
 
 export const purchaseBurger = (orderData, token) => {
   return (dispatch) => {
-    dispatch(purchaseBurgerStart())
+    dispatch(purchaseBurgerStart());
     axios
-      .post('/orders.json?auth=', orderData)
+      .post('/orders.json?auth=' + token, orderData)
       .then((response) => {
         console.log(response.data);
         dispatch(purchaseBurgerSuccess(response.data.name, orderData));
@@ -38,52 +37,53 @@ export const purchaseBurger = (orderData, token) => {
   };
 };
 
-export const purchaseInit = () =>{
-  return{
-    type:actionTypes.PURCHASE_INIT
-  }
-}
-
-
-export const fetchOrdersSuccess =(orders) =>{
-  return{
-    type:actionTypes.FETCH_ORDERS_SUCCESS,
-    orders:orders
-  }
-}
-
-
-export  const fetchOrdersFail =(error)=>{
-  return{
-    type:actionTypes.FETCH_ORDERS_FAIL,
-    error:error
-  }
-}
-
-export const fetchOrdersStart = () =>{
+export const purchaseInit = () => {
   return {
-    type:actionTypes.FETCH_ORDERS_START
-  }
-}
+    type: actionTypes.PURCHASE_INIT,
+  };
+};
 
+export const fetchOrdersSuccess = (orders) => {
+  return {
+    type: actionTypes.FETCH_ORDERS_SUCCESS,
+    orders: orders,
+  };
+};
 
-export const fetchOrders =(token) =>{
-  return dispatch=>{
+export const fetchOrdersFail = (error) => {
+  return {
+    type: actionTypes.FETCH_ORDERS_FAIL,
+    error: error,
+  };
+};
+
+export const fetchOrdersStart = () => {
+  return {
+    type: actionTypes.FETCH_ORDERS_START,
+  };
+};
+
+export const fetchOrders = (token, userId) => {
+  console.log("UserId from fetchOrder in order action", userId)
+  return (dispatch) => {
     dispatch(fetchOrdersStart())
-    axios.get('/orders.json?auth=' +token)
-    .then(res => {
+    const queryParams =
+      '?auth=' + token + '&orderBy="userId"&equalTo="' + userId + '"';
+    dispatch(fetchOrdersStart());
+    axios
+      .get('/orders.json?auth=' + queryParams)
+      .then((res) => {
         const fetchedOrders = [];
         for (let key in res.data) {
-            fetchedOrders.push({
-                ...res.data[key],
-                id: key
-            });
+          fetchedOrders.push({
+            ...res.data[key],
+            id: key,
+          });
         }
-        dispatch(fetchOrdersSuccess(fetchedOrders))
-      
-    })
-    .catch(err => {
-       dispatch(fetchOrdersFail(err))
-    });
-  }
-}
+        dispatch(fetchOrdersSuccess(fetchedOrders));
+      })
+      .catch((err) => {
+        dispatch(fetchOrdersFail(err));
+      });
+  };
+};
